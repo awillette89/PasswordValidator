d7a14ae2e8eefd78c95ddd011346f2ec3be8dce0
awillette89 <awillette@gmail.com>
2025-04-05 15:55:31 -0400

    Added initial gathering of user input

---
+++ PasswordValidator.py
@@ -0,0 +1,11 @@
+print("Password Requirements: "
+"- Contains 8-16 characters\n"
+"- Contains one of the special characters: %$#^&*!@()\n"
+"- Contains at least one number 0-9\n"
+"- Contains at least one capital letter\n"
+"- Starts with a lowercase letter\n"
+"- Does not contain the phrase 'pass' in any format\n"
+"- Does not contain the phrase 'qwerty' in any format\n"
+"- Does not contain the phrase '123'")
+
+user_input = input ("Enter a password that meetes the requirements: ")
